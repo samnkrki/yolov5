39e154c71e1dacc4ac07afea88d22643a6813908
Samin Karki <samn_krki@live.com>
2023-07-20 09:25:56 +0545

    kmp duplicate settings added

--- train.py
+++ train.py
@@ -65,7 +65,7 @@ LOCAL_RANK = int(os.getenv('LOCAL_RANK', -1))  # https://pytorch.org/docs/stable
 RANK = int(os.getenv('RANK', -1))
 WORLD_SIZE = int(os.getenv('WORLD_SIZE', 1))
 GIT_INFO = check_git_info()
-
+os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
 
 def train(hyp, opt, device, callbacks):  # hyp is path/to/hyp.yaml or hyp dictionary
     save_dir, epochs, batch_size, weights, single_cls, evolve, data, cfg, resume, noval, nosave, workers, freeze = \
